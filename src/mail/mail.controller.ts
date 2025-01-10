83360cd46840ac61dcb4a30b24374f7918b8c6b7
hyosoek <tennfin1@gmail.com>
2025-01-10 14:45:15 +0900

    feat(swagger) : add swagger code with dto

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SendMailDto } from './mail.dto';
 
+@ApiTags('mail Server API')
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
+  @ApiOperation({ summary: '메일 전송 테스트' })
+  @ApiResponse({ status: 201, description: '성공적으로 메일 전송 완료' })
   @Post()
-  async sendMail(
-    @Body() body: { address: string; mailTitle: string; mailContents: string },
-  ) {
+  async sendMail(@Body() body: SendMailDto) {
     try {
       await this.mailService.sendEmail(
         body.address,
